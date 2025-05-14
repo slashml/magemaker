e5569e77bf4ac59eea27fcb49c3d4d8fe180d240
AbdullahSharif <abdullah.29.umar@gmail.com>
2025-05-14 12:05:53 +0500

    test 4

--- magemaker/runner.py
+++ magemaker/runner.py
@@ -188,6 +188,22 @@ if __name__ == '__main__':
         help="path to YAML training configuration file",
         type=str
     )
+
+
+
+    # trying to test:
+    parser.add_argument(
+        "--deploy",
+        help="path to YAML deployment configuration file",
+        type=str
+    )
+    parser.add_argument(
+        "--train",
+        help="path to YAML training configuration file",
+        type=str
+    )
+
+    
     parser.add_argument(
         "-v",
         "--verbose",
