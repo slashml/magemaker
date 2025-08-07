50f25b7d5c82753899a4d7acae9176d36d311b22
eff-kay <faizan10114@gmail.com>
2025-08-07 15:19:42 -0400

    specify the cpu type

--- magemaker/runner.py
+++ magemaker/runner.py
@@ -44,11 +44,19 @@ def runner():
         help="Deploy a Hugging Face Model.",
         type=str
     )
+
     parser.add_argument(
         "--instance",
         help="EC2 instance type to deploy to.",
         type=str
     )
+
+    parser.add_argument(
+        "--cpu",
+        help="specify cpu type",
+        type=str
+    )
+
     parser.add_argument(
         "--deploy",
         help="path to YAML deployment configuration file",
