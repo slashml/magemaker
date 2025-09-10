2c11cd7905f382b473dfdc52c51b50d4c7827b01
eff-kay <faizan10114@gmail.com>
2025-09-10 18:59:42 -0400

    add modal lab support

--- magemaker/runner.py
+++ magemaker/runner.py
@@ -202,7 +202,7 @@ if __name__ == '__main__':
     
     parser.add_argument(
         "--cloud", 
-        choices=['aws', 'gcp', 'azure', 'IBM', 'cisco', 'hetzner', 'ovs', 'digital-ocean'], 
+        choices=['aws', 'gcp', 'azure', 'IBM', 'cisco', 'hetzner', 'ovs', 'digital-ocean', 'modal-labs'], 
         help="Specify the cloud provider for configuration and deployment"
     )
 
